298406da22b933c36f83ffdb688ecf90bf215e09
Admin-K <84641391+Devloper90@users.noreply.github.com>
2021-11-17 11:20:41 +0530

    loi first

---
+++ index.js
@@ -0,0 +1,7 @@
+$("button.btn").on("click",function(){
+    prompt("ENTER YOUR NAME")
+    alert("This page has my IP address DHOOND LO...")
+    $("button.btn").css("visibility","hidden")
+    $("video#back").css("visibility","visible")
+})
+
